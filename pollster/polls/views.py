db56e5ee09150dc11e355277c1508c0d8ab1bbca
enzorooo <ph.enzomercado@gmail.com>
2020-10-13 15:12:12 +0800

    updating repo with latest changes

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
